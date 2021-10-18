9a42b082e277583cc300e7ad3388aace5ea39312
prashanthpasham <prashanth.pasham15@gmail.com>
2021-10-18 12:18:18 +0530

    forms-app

---
+++ Forms-App/src/main/java/com/prashanth/FormsAppApplication.java
@@ -0,0 +1,13 @@
+package com.prashanth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormsAppApplication.class, args);
+	}
+
+}
